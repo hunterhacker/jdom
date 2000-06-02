66c7a96eddc98d3bd7d7358a27da34f1a73d0383
bmclaugh <bmclaugh>
2000-06-02 19:19:44 +0000

    Updated Builder to be deprecated

--- core/src/java/org/jdom/input/Builder.java
+++ core/src/java/org/jdom/input/Builder.java
@@ -1,43 +1,43 @@
-/*-- 
+/*--
 
- Copyright (C) @year@ Brett McLaughlin & Jason Hunter. All rights reserved.
- 
+ Copyright (C) 2000 Brett McLaughlin & Jason Hunter. All rights reserved.
+
  Redistribution and use in source and binary forms, with or without modifica-
  tion, are permitted provided that the following conditions are met:
- 
+
  1. Redistributions of source code must retain the above copyright notice,
     this list of conditions, and the following disclaimer.
- 
+
  2. Redistributions in binary form must reproduce the above copyright notice,
     this list of conditions, the disclaimer that follows these conditions,
     and/or other materials provided with the distribution.
- 
+
  3. The names "JDOM" and "Java Document Object Model" must not be used to
     endorse or promote products derived from this software without prior
     written permission. For written permission, please contact
     license@jdom.org.
- 
+
  4. Products derived from this software may not be called "JDOM", nor may
     "JDOM" appear in their name, without prior written permission from the
     JDOM Project Management (pm@jdom.org).
- 
+
  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
- JDOM PROJECT  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT, INDIRECT, 
- INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLUDING, BUT 
+ JDOM PROJECT  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT, INDIRECT,
+ INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLUDING, BUT
  NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
  OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
  ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
  (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- 
+
  This software  consists of voluntary contributions made  by many individuals
- on  behalf of the Java Document Object Model Project and was originally 
- created by Brett McLaughlin <brett@jdom.org> and 
- Jason Hunter <jhunter@jdom.org>. For more  information on the JDOM 
+ on  behalf of the Java Document Object Model Project and was originally
+ created by Brett McLaughlin <brett@jdom.org> and
+ Jason Hunter <jhunter@jdom.org>. For more  information on the JDOM
  Project, please see <http://www.jdom.org/>.
- 
+
  */
 package org.jdom.input;
 
@@ -57,6 +57,8 @@ import org.jdom.JDOMException;
  *   supported.
  * </p>
  *
+ * @deprecated <code>{@link SAXBuilder}</code> and <code>{@link DOMBuilder}</code>
+ *             should now be used directly, instead of through this interface.
  * @author <a href="mailto:brett@jdom.org">Brett McLaughlin</a>
  * @author <a href="mailto:jhunter@jdom.org">Jason Hunter</a>
  * @version 1.0
@@ -73,10 +75,10 @@ public interface Builder {
      * @param in <code>InputStream</code> to read from.
      * @return <code>Document</code> - resultant Document object.
      * @throws <code>JDOMException</code> when errors occur in
-     *                                    parsing.     
-     */    
+     *                                    parsing.
+     */
     public Document build(InputStream in)
-        throws JDOMException;       
+        throws JDOMException;
 
     /**
      * <p>
@@ -87,7 +89,7 @@ public interface Builder {
      * @param file <code>File</code> to read from.
      * @return <code>Document</code> - resultant Document object.
      * @throws <code>JDOMException</code> when errors occur in
-     *                                    parsing.     
+     *                                    parsing.
      */
     public Document build(File file) throws JDOMException;
 
@@ -100,8 +102,8 @@ public interface Builder {
      * @param url <code>URL</code> to read from.
      * @return <code>Document</code> - resultant Document object.
      * @throws <code>JDOMException</code> when errors occur in
-     *                                    parsing.     
+     *                                    parsing.
      */
     public Document build(URL url) throws JDOMException;
-    
+
 }
