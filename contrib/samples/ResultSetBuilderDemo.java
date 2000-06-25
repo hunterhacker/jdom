4881a5199a8e0986d26bb73e682d6571c3ceb87c
jhunter <jhunter>
2000-06-25 18:50:13 +0000

    Contributing my own ResultSetBuilder and Demo.

---
+++ contrib/samples/ResultSetBuilderDemo.java
@@ -0,0 +1,113 @@
+/*-- 
+
+ Copyright 2000 Brett McLaughlin & Jason Hunter. All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modifica-
+ tion, are permitted provided that the following conditions are met:
+ 
+ 1. Redistributions of source code must retain the above copyright notice,
+    this list of conditions, and the following disclaimer.
+ 
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions, the disclaimer that follows these conditions,
+    and/or other materials provided with the distribution.
+ 
+ 3. The names "JDOM" and "Java Document Object Model" must not be used to
+    endorse or promote products derived from this software without prior
+    written permission. For written permission, please contact
+    license@jdom.org.
+ 
+ 4. Products derived from this software may not be called "JDOM", nor may
+    "JDOM" appear in their name, without prior written permission from the
+    JDOM Project Management (pm@jdom.org).
+ 
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ JDOM PROJECT  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT, INDIRECT, 
+ INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLUDING, BUT 
+ NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ This software  consists of voluntary contributions made  by many individuals
+ on  behalf of the Java Document Object Model Project and was originally 
+ created by Brett McLaughlin <brett@jdom.org> and 
+ Jason Hunter <jhunter@jdom.org>. For more  information on the JDOM 
+ Project, please see <http://www.jdom.org/>.
+ 
+ */
+
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+
+import org.jdom.*;
+import org.jdom.output.*;
+
+import org.jdom.contrib.input.ResultSetBuilder;
+
+public class ResultSetBuilderDemo {
+    
+  // SQL tables copied from the Servlets.com ISP listing application
+
+  static final String PREP = 
+    "create table rsbd ( " +
+    "id int PRIMARY KEY, " +
+    "name varchar(255) NOT NULL, " +
+    "home_url varchar(255) NULL, " +
+    "contact_email varchar(255) NULL, " +
+    "contact_phone varchar(255) NULL, " +
+    "location varchar(255) NULL, " +
+    "comments long varchar NULL, " +
+    "free_hosting boolean NULL, " +
+    "state_flag tinyint NOT NULL, " + // submitted, rejected, live, dead
+    "submitter_email varchar(255) NULL, " + // not displayed
+    "created_time timestamp NOT NULL, " +
+    "modified_time timestamp NOT NULL " +
+    ")";
+
+  static final String FILL =
+    "insert into rsbd (id, name, home_url, contact_email, " +
+    "contact_phone, comments, free_hosting, state_flag, created_time, " +
+    "modified_time) values (2, 'sphere', null, 'info@sphere', " +
+    "'1234', 'cool', true, 10, " +
+    "{ts '1999-02-09 20:11:11.123455'}, " +
+    "{ts '1999-03-21 22:11:11.123455'})";
+
+  public static void main(String[] args) throws Exception {
+    // Tested against Cloudscape database that comes with the J2EE ref impl
+    Class.forName("COM.cloudscape.core.JDBCDriver");
+    Connection con =
+      DriverManager.getConnection("jdbc:cloudscape:rsbd;create=true");
+
+/*  
+    // Create and fill commands, needed only on the first run
+    Statement prep = con.createStatement();
+    prep.executeUpdate(PREP);
+
+    Statement fill = con.createStatement();
+    fill.executeUpdate(FILL);
+*/
+
+    Namespace ns = Namespace.getNamespace("xhtml", "http://w3.org/etc");
+
+    Statement stmt = con.createStatement();
+    ResultSet rs = stmt.executeQuery(
+      "select id, name, home_url || contact_phone from rsbd");
+    ResultSetBuilder builder = new ResultSetBuilder(rs);
+    builder.setAsElement(3, "num3");
+    //builder.setNamespace(ns);
+    //builder.setAsElement("id", "newid");
+    //builder.setAsElement("home_url", "newhome_url");
+    //builder.setAsElement(4, "some4");
+    //builder.setAsAttribute(4, "some4");
+    //builder.setAsAttribute("state_flag");
+    builder.setAsAttribute("created_time", "ctime");
+    Document doc = builder.build();
+    XMLOutputter outputter = new XMLOutputter();
+    outputter.output(doc, System.out);
+  }
+}
---
+++ contrib/src/java/org/jdom/contrib/input/ResultSetBuilder.java
@@ -0,0 +1,231 @@
+/*-- 
+
+ Copyright 2000 Brett McLaughlin & Jason Hunter. All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modifica-
+ tion, are permitted provided that the following conditions are met:
+ 
+ 1. Redistributions of source code must retain the above copyright notice,
+    this list of conditions, and the following disclaimer.
+ 
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions, the disclaimer that follows these conditions,
+    and/or other materials provided with the distribution.
+ 
+ 3. The names "JDOM" and "Java Document Object Model" must not be used to
+    endorse or promote products derived from this software without prior
+    written permission. For written permission, please contact
+    license@jdom.org.
+ 
+ 4. Products derived from this software may not be called "JDOM", nor may
+    "JDOM" appear in their name, without prior written permission from the
+    JDOM Project Management (pm@jdom.org).
+ 
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ JDOM PROJECT  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT, INDIRECT, 
+ INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLUDING, BUT 
+ NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ This software  consists of voluntary contributions made  by many individuals
+ on  behalf of the Java Document Object Model Project and was originally 
+ created by Brett McLaughlin <brett@jdom.org> and 
+ Jason Hunter <jhunter@jdom.org>. For more  information on the JDOM 
+ Project, please see <http://www.jdom.org/>.
+ 
+ */
+
+package org.jdom.contrib.input;
+
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+
+import org.jdom.*;
+
+/**
+ * <p><code>ResultSetBuilder</code> builds a JDOM tree from a 
+ * <code>java.sql.ResultSet</code>.  Many good ideas were leveraged from
+ * SQLBuilder written from Jon Baer.</p>
+ *
+ * Notes:
+ *   Uses name returned by rsmd.getColumnName(), not getColumnLabel() 
+ *   because that is less likely to be a valid XML element title.
+ *   Null values are given empty bodies.  Be aware that databases may 
+ *   change the case of column names.  setAsXXX() methods are case
+ *   insensitive on input column name.  Assign each one a proper output name
+ *   if you're worried.  Only build() throws JDOMException.  Any exceptions 
+ *   encountered in the set methods are thrown during the build().  
+ *   The setAsXXX(String columnName, ...) methods do not verify that a column 
+ *   with the given name actually exists.
+ *   Still needs method-by-method Javadocs.
+ * <p>
+ * Issues: 
+ *   Do attributes have to be added in a namespace?
+ *
+ * @author Jason Hunter
+ * @author Jon Baer
+ * @version 0.5
+ */
+public class ResultSetBuilder {
+    
+    ResultSet rs;
+    ResultSetMetaData rsmd;
+    SQLException exception = null;
+
+    Map names = new HashMap();   // Maps original names to display names
+    Map attribs = new HashMap(); // Maps original names to attrib or not
+
+    Namespace ns = Namespace.NO_NAMESPACE;  // default to none
+    int maxRows = Integer.MAX_VALUE;        // default to all
+    String rootName = "result";
+    String rowName = "entry";
+
+
+    public ResultSetBuilder(ResultSet rs) {
+      this.rs = rs;
+      try {
+        rsmd = rs.getMetaData();
+      }
+      catch (SQLException e) {
+        // Hold the exception until build() is called
+        exception = e;
+      }
+    }
+
+    public Document build() throws JDOMException {
+      if (exception != null) {
+        throw new JDOMException("Database problem", exception);
+      }
+
+      try {
+        int colCount = rsmd.getColumnCount();
+
+        Element root = new Element(rootName, ns);
+        Document doc = new Document(root);
+
+        int rowCount = 0;
+        while (rs.next() && (rowCount++ < maxRows)) {
+          Element entry = new Element(rowName, ns);
+          for (int col = 1; col <= colCount; col++) {
+            String origName = rsmd.getColumnName(col);
+            String name = lookupName(origName);
+            String value = rs.getString(col);
+            if (isAttribute(origName)) {
+              entry.addAttribute(name, value);
+            }
+            else {
+              Element child = new Element(name, ns);
+              if (!rs.wasNull()) {
+                child.setContent(value);
+              }
+              entry.addChild(child);
+            }
+          }
+          root.addChild(entry);
+        }
+
+        return doc;
+      }
+      catch (SQLException e) {
+        throw new JDOMException("Database problem", e);
+      }
+    }
+
+    private String lookupName(String origName) {
+      String name = (String) names.get(origName.toLowerCase());
+      if (name != null) {
+        return name;
+      }
+      else {
+        return origName;
+      }
+    }
+
+    private boolean isAttribute(String origName) {
+      Boolean val = (Boolean) attribs.get(origName.toLowerCase());
+      if (val == Boolean.TRUE) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+
+    public void setRootName(String rootName) {
+      this.rootName = rootName;
+    }
+
+    public void setRowName(String rowName) {
+      this.rowName = rowName;
+    }
+
+    public void setNamespace(Namespace ns) {
+      this.ns = ns;
+    }
+
+    public void setMaxRows(int maxRows) {
+      this.maxRows = maxRows;
+    }
+
+    public void setAsAttribute(String columnName) {
+      attribs.put(columnName.toLowerCase(), Boolean.TRUE);
+    }
+
+    public void setAsAttribute(String columnName, String attribName) {
+      attribs.put(columnName.toLowerCase(), Boolean.TRUE);
+      names.put(columnName.toLowerCase(), attribName);
+    }
+
+    public void setAsAttribute(int columnNum) {
+      try {
+        String name = rsmd.getColumnName(columnNum).toLowerCase();
+        attribs.put(name, Boolean.TRUE);
+      }
+      catch (SQLException e) {
+        exception = e;
+      }
+    }
+
+    public void setAsAttribute(int columnNum, String attribName) {
+      try {
+        String name = rsmd.getColumnName(columnNum).toLowerCase();
+        attribs.put(name, Boolean.TRUE);
+        names.put(name, attribName);
+      }
+      catch (SQLException e) {
+        exception = e;
+      }
+    }
+
+    public void setAsElement(String columnName, String elemName) {
+      String name = columnName.toLowerCase();
+      attribs.put(name, Boolean.FALSE);
+      names.put(name, elemName);
+    }
+
+    public void setAsElement(int columnNum, String elemName) {
+      try {
+        String name = rsmd.getColumnName(columnNum).toLowerCase();
+        attribs.put(name, Boolean.FALSE);
+        names.put(name, elemName);
+      }
+      catch (SQLException e) {
+        exception = e;
+      }
+    }
+
+/*
+    public void setAsIngore(String columnName) {
+    }
+
+    public void setAsIngore(int columnNum) {
+    }
+*/
+
+}
