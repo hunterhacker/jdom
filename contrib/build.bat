998ab107b3fbd1954787ee04d25924d6fe35d835
jhunter <jhunter>
2000-07-11 22:36:03 +0000

    Keeping build.bat in line with build.sh for antlr.

--- contrib/build.bat
+++ contrib/build.bat
@@ -4,7 +4,7 @@ echo Building...
 
 if "%JAVA_HOME%" == "" goto error
 
-set LOCALCLASSPATH=%JAVA_HOME%\lib\tools.jar;..\jdom\lib\xerces.jar;..\jdom\lib\ant.jar;..\jdom\build\classes;.\taskdefs\taskdefs.jar;.\lib\antlr.jar;%CLASSPATH%
+set LOCALCLASSPATH=%JAVA_HOME%\lib\tools.jar;..\jdom\lib\xerces.jar;..\jdom\lib\ant.jar;..\jdom\build\classes;.\taskdefs\taskdefs.jar;.\lib\antlr_full.jar;%CLASSPATH%
 set ANT_HOME=../jdom/lib
 
 echo Building with classpath %LOCALCLASSPATH%
