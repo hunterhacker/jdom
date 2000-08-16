6b2ceea20246638932f3837bbe493fd8c31ac81b
jools <jools>
2000-08-16 09:52:33 +0000

    Missed org.jdom.Attribute on the first pass of test case generation.
    Assigned the task of implementing the tests to Philip Nelson <philip.nelson@omniresources.com>

---
+++ test/src/java/org/jdom/test/cases/TCM_Attribute_long_getLongValue.java
@@ -0,0 +1,41 @@
+/* Please run replic.pl on me ! */
+package org.jdom.test.cases;
+
+/**
+ * Please put a description of your test here.
+ * 
+ * @author unascribed
+ * @version 0.1
+ */
+public final class TCM_Attribute_long_getLongValue
+extends junit.framework.TestCase
+{
+    /**
+     *  Construct a new instance. 
+     */
+    public TCM_Attribute_long_getLongValue() {
+        super("public long org.jdom.Attribute.getLongValue() throws org.jdom.DataConversionException");
+    }
+
+    /**
+     * This method is called before a test is executed.
+     */
+    public void setUp() {
+        // your code goes here.
+    }
+
+    /**
+     * This method is called after a test is executed.
+     */
+    public void tearDown() {
+        // your code goes here.
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test() {
+        fail("implement me !");
+    }
+
+}
