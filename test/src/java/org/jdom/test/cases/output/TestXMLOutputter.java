e5c23d80ab9e78610b8251ba8e263898ff9d8dea
nelsonp <nelsonp>
2001-06-27 04:05:11 +0000

    Unimplemented test cases as a shell where Alex can add his tests

---
+++ test/src/java/org/jdom/test/cases/output/TestXMLOutputter.java
@@ -0,0 +1,367 @@
+/* Please run replic.pl on me ! */
+package org.jdom.test.cases.output;
+
+/**
+ * Please put a description of your test here.
+ * 
+ * @author unascribed
+ * @version 0.1
+ */
+import junit.framework.*;
+
+public final class TestXMLOutputter
+extends junit.framework.TestCase
+{
+    /**
+     *  Construct a new instance. 
+     */
+    public TestXMLOutputter(String name) {
+        super(name);
+    }
+
+    /**
+     * This method is called before a test is executed.
+     */
+    public void setUp() {
+        // your code goes here.
+    }
+
+    /**
+     * This method is called after a test is executed.
+     */
+    public void tearDown() {
+        // your code goes here.
+    }
+
+    /**
+     * The suite method runs all the tests
+     */
+public static Test suite () {
+        TestSuite suite = new TestSuite(TestXMLOutputter.class);
+        return suite;
+    }
+
+    /**
+     * The main method runs all the tests in the text ui
+     */
+    public static void main (String args[]) 
+     {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCC__() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCC___OrgJdomOutputXMLOutputter() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCC___String() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCC___String_boolean() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCC___String_boolean_String() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_output_OrgJdomComment_OutputStream() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_setIndent_boolean() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__String_outputString_OrgJdomEntityRef() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_output_OrgJdomCDATA_OutputStream() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_output_OrgJdomDocument_Writer() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_output_OrgJdomDocType_OutputStream() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__String_outputString_OrgJdomProcessingInstruction() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_setIndentLevel_int() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_outputElementContent_OrgJdomElement_Writer() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_setNewlines_boolean() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_output_OrgJdomProcessingInstruction_OutputStream() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_output_OrgJdomCDATA_Writer() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_setPadText_boolean() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_setExpandEmptyElements_boolean() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_setOmitEncoding_boolean() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__String_outputString_OrgJdomCDATA() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_setIndent_String() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_setTextNormalize_boolean() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_setEncoding_String() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_outputElementContent_OrgJdomElement_OutputStream() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__String_outputString_OrgJdomElement() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_output_OrgJdomComment_Writer() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__String_outputString_OrgJdomComment() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_setTrimText_boolean() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__int_parseArgs_ArrayString_int() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__String_outputString_OrgJdomDocType() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_output_String_Writer() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_output_OrgJdomElement_OutputStream() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_output_OrgJdomDocument_OutputStream() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_output_OrgJdomElement_Writer() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_output_OrgJdomDocType_Writer() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_output_OrgJdomEntityRef_OutputStream() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_setLineSeparator_String() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__String_outputString_OrgJdomDocument() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_setOmitDeclaration_boolean() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_output_OrgJdomEntityRef_Writer() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_setSuppressDeclaration_boolean() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_setIndentSize_int() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_output_String_OutputStream() {
+        fail("implement me !");
+    }
+
+    /**
+     * Test code goes here. Replace this comment.
+     */
+    public void test_TCM__void_output_OrgJdomProcessingInstruction_Writer() {
+        fail("implement me !");
+    }
+
+}
