77be4bad981f6f2e906c4aae369c2385409fd98a
nelsonp <nelsonp>
2002-04-24 20:47:24 +0000

    Simplified the path and left only those things that are required for Ant to run. The exception is Xalan which the junit report task needs on the system classpath.  Other tests/compiles will use libraries based in the build.xml file. For the unix build.sh I left in the entries that I couln't test until I get freeBSD running correctly.

--- test/build.sh
+++ test/build.sh
@@ -12,7 +12,7 @@ if [ "$JAVA_HOME" = "" ] ; then
   exit 1
 fi
 
-LOCALCLASSPATH=$JAVA_HOME/lib/tools.jar:./lib/junit.jar:../jdom/lib/xerces.jar:./lib/ant.jar:./lib/jakarta-ant-1.3-optional.jar:../jdom/build/classes:../jdom-contrib/build/classes:$JAVA_HOME/lib/dev.jar:$ADDITIONALCLASSPATH
+LOCALCLASSPATH=$JAVA_HOME/lib/tools.jar:./lib/junit.jar:../jdom/lib/xerces.jar:./lib/ant.jar:./lib/optional.jar:./lib/xalan.jar:../jdom/build/classes:../jdom-contrib/build/classes:$JAVA_HOME/lib/dev.jar:$ADDITIONALCLASSPATH
 ANT_HOME=./lib
 
 echo Building with classpath $CLASSPATH:$LOCALCLASSPATH:$ADDITIONALCLASSPATH
