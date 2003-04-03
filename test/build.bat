b04031d2ad9935e56534e16d9af1900e4135ca0e
jhunter <jhunter>
2003-04-03 00:29:20 +0000

    Moving the classpath into the build.xml file.  Also updated the optional.jar for Ant 1.5.2 with an accompanying readme file.
    -jh-

--- test/build.bat
+++ test/build.bat
@@ -4,7 +4,7 @@ echo Building...
 
 if "%JAVA_HOME%" == "" goto error
 
-set LOCALCLASSPATH=%JAVA_HOME%\lib\tools.jar;.\lib\junit.jar;..\jdom\lib\xerces.jar;..\jdom\lib\xml-apis.jar;..\jdom\lib\ant.jar;.\lib\optional.jar;..\jdom\lib\xalan.jar;..\jdom\build\classes;..\jdom-contrib\build\classes
+set LOCALCLASSPATH=%JAVA_HOME%\lib\tools.jar;..\jdom\lib\ant.jar;..\jdom\lib\xml-apis.jar;..\jdom\lib\xerces.jar;.\lib\optional.jar;.\lib\junit.jar
 set ANT_HOME=..\jdom\lib
 
 echo Building with classpath %LOCALCLASSPATH%;%ADDITIONALCLASSPATH%
