2035c3650594a7aed98923c1f2c7bb32dac0fb8b
jhunter <jhunter>
2003-04-04 20:30:02 +0000

    Updating JUnit to 3.8.1 so "build testui" will work.  Added accompanying readme and license files for JUnit.  Also moved the junit.jar classpath reference into the build.xml file.
    -jh-

--- test/build.sh
+++ test/build.sh
@@ -18,7 +18,7 @@ else
   PS=":"
 fi
 
-LOCALCLASSPATH=${JAVA_HOME}/lib/tools.jar${PS}${JAVA_HOME}/lib/dev.jar${PS}../jdom/lib/ant.jar${PS}../jdom/lib/xml-apis.jar${PS}../jdom/lib/xerces.jar${PS}./lib/optional.jar${PS}./lib/junit.jar
+LOCALCLASSPATH=${JAVA_HOME}/lib/tools.jar${PS}${JAVA_HOME}/lib/dev.jar${PS}../jdom/lib/ant.jar${PS}../jdom/lib/xml-apis.jar${PS}../jdom/lib/xerces.jar${PS}./lib/optional.jar
 ANT_HOME=../jdom/lib
 
 echo Building with classpath $LOCALCLASSPATH${PS}$ADDITIONALCLASSPATH${PS}$CLASSPATH
