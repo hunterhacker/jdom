797cb06078866fdc4ab77428e784969eef1bad8e
jhunter <jhunter>
2006-11-16 08:34:18 +0000

    Last checkin fixed usage.  This one makes sure we're pretty printing.
    -jh-

--- core/samples/SAXBuilderDemo.java
+++ core/samples/SAXBuilderDemo.java
@@ -1,6 +1,6 @@
 /*--
 
- $Id: SAXBuilderDemo.java,v 1.18 2006/11/16 08:32:39 jhunter Exp $
+ $Id: SAXBuilderDemo.java,v 1.19 2006/11/16 08:34:18 jhunter Exp $
 
  Copyright (C) 2000-2004 Jason Hunter & Brett McLaughlin.
  All rights reserved.
@@ -119,11 +119,11 @@ public class SAXBuilderDemo {
                 builder = new SAXBuilder(saxDriverClass);
             }
             builder.setExpandEntities(expandEntities);
-//builder.setIgnoringBoundaryWhitespace(true);
+            //builder.setIgnoringBoundaryWhitespace(true);
 
             Document doc = builder.build(filename);
 
-            XMLOutputter outputter = new XMLOutputter();
+            XMLOutputter outputter = new XMLOutputter(Format.getPrettyFormat());
             //outputter.setExpandEmptyElements(true);
             outputter.output(doc, System.out);
         } catch (JDOMException e) {
