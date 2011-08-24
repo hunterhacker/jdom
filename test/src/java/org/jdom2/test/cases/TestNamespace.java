6dd2d1225d048772c5a3bc7734f424202fb4d0e6
Rolf <rolf@tuis.net>
2011-08-24 13:41:24 -0400

    Complete the Namespace test coverage.

--- test/src/java/org/jdom2/test/cases/TestNamespace.java
+++ test/src/java/org/jdom2/test/cases/TestNamespace.java
@@ -4,14 +4,14 @@ package org.jdom2.test.cases;
 
  Copyright (C) 2000 Brett McLaughlin & Jason Hunter.
  All rights reserved.
- 
+
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions
  are met:
- 
+
  1. Redistributions of source code must retain the above copyright
 	notice, this list of conditions, and the following disclaimer.
- 
+
  2. Redistributions in binary form must reproduce the above copyright
 	notice, this list of conditions, and the disclaimer that follows 
 	these conditions in the documentation and/or other materials 
@@ -20,11 +20,11 @@ package org.jdom2.test.cases;
  3. The name "JDOM" must not be used to endorse or promote products
 	derived from this software without prior written permission.  For
 	written permission, please contact license@jdom.org.
- 
+
  4. Products derived from this software may not be called "JDOM", nor
 	may "JDOM" appear in their name, without prior written permission
 	from the JDOM Project Management (pm@jdom.org).
- 
+
  In addition, we request (but do not require) that you include in the 
  end-user documentation provided with the redistribution and/or in the 
  software itself an acknowledgement equivalent to the following:
@@ -51,7 +51,7 @@ package org.jdom2.test.cases;
  created by Brett McLaughlin <brett@jdom.org> and 
  Jason Hunter <jhunter@jdom.org>.  For more information on the 
  JDOM Project, please see <http://www.jdom.org/>.
- 
+
  */
 
 /**
@@ -60,6 +60,8 @@ package org.jdom2.test.cases;
  * @author unascribed
  * @version 0.1
  */
+import javax.xml.XMLConstants;
+
 import org.jdom2.*;
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
@@ -67,48 +69,48 @@ import static org.junit.Assert.*;
 
 public final class TestNamespace {
 
-    /**
-     * The main method runs all the tests in the text ui
-     */
-    public static void main (String args[]) 
-     {
-        JUnitCore.runClasses(TestNamespace.class);
-    }
+	/**
+	 * The main method runs all the tests in the text ui
+	 */
+	public static void main (String args[]) 
+	{
+		JUnitCore.runClasses(TestNamespace.class);
+	}
 
 	/**
 	 * Test the object comparison method.
 	 */
-    @Test
+	@Test
 	public void test_TCM__boolean_equals_Object() {
 		Namespace ns = Namespace.getNamespace("prefx", "http://some.other.place");
 		Object ob = (Object)ns;
-	    assertTrue("object not equal to attribute", ns.equals(ob));
+		assertTrue("object not equal to attribute", ns.equals(ob));
 
 		ns = Namespace.NO_NAMESPACE;
 		ob = (Object)ns;
-	    assertTrue("object not equal to attribute", ns.equals(ob));
+		assertTrue("object not equal to attribute", ns.equals(ob));
 
 		//ns = Namespace.EMPTY_NAMESPACE;
 		//ob = (Object)ns;
-	    //assertTrue("object not equal to attribute", ns.equals(ob));
-	    
-	    
+		//assertTrue("object not equal to attribute", ns.equals(ob));
+
+
 	}
 
-    /**
+	/**
 	 * Verify that a namespace will produce a hashcode.
 	 */
-    @Test
+	@Test
 	public void test_TCM__int_hashCode() {
 		Namespace ns = Namespace.getNamespace("test", "value");
 		//only an exception would be a problem
-                int i = -1;
-                try {
-                        i = ns.hashCode();
-                }
-                catch(Exception e) {
-                        fail("bad hashCode");
-                }
+		int i = -1;
+		try {
+			i = ns.hashCode();
+		}
+		catch(Exception e) {
+			fail("bad hashCode");
+		}
 
 		//make sure a new one doesn't have the same value
 		Namespace ns2 = Namespace.getNamespace("test", "value2");
@@ -117,12 +119,14 @@ public final class TestNamespace {
 
 		//test hashcode for NO_NAMESPACE
 		//only an exception would be a problem
-                try {
-		        int y = Namespace.NO_NAMESPACE.hashCode();
-                }
-                catch(Exception e) {
-                        fail("bad hashCode");
-                }
+		try {
+			// Namespace.hashCode() is uri.hashCode().
+			// NO_NAMESPACE has URI ""
+			assertTrue(Namespace.NO_NAMESPACE.hashCode() == "".hashCode());
+		}
+		catch(Exception e) {
+			fail("bad hashCode");
+		}
 
 		//test hashcode for NO_NAMESPACE
 		//y = Namespace.EMPTY_NAMESPACE.hashCode();
@@ -133,7 +137,7 @@ public final class TestNamespace {
 	/**
 	 * Test the URI only Namespace.
 	 */
-    @Test
+	@Test
 	public void test_TCM__OrgJdomNamespace_getNamespace_String() {
 		Namespace ns = Namespace.getNamespace("http://some.new.place");
 		assertTrue("Incorrect namespace created", ns.toString().equals("[Namespace: prefix \"\" is mapped to URI \"http://some.new.place\"]"));
@@ -143,20 +147,84 @@ public final class TestNamespace {
 
 	}
 
-    /**
+	/**
 	 * Test the prefix, uri version of getNamespace.
 	 */
-    @Test
+	@Test
 	public void test_TCM__OrgJdomNamespace_getNamespace_String_String() {
 		Namespace ns = Namespace.getNamespace("prefx", "http://some.other.place");
 		assertTrue("Incorrect namespace created", ns.toString().equals("[Namespace: prefix \"prefx\" is mapped to URI \"http://some.other.place\"]"));
 
+		assertTrue(Namespace.NO_NAMESPACE == Namespace.getNamespace(""));
+		assertTrue(Namespace.NO_NAMESPACE == Namespace.getNamespace(null));
+		assertTrue(Namespace.NO_NAMESPACE == Namespace.getNamespace(null, null));
+		assertTrue(Namespace.NO_NAMESPACE == Namespace.getNamespace(null, ""));
+		assertTrue(Namespace.NO_NAMESPACE == Namespace.getNamespace("", ""));
+		assertTrue(Namespace.NO_NAMESPACE == Namespace.getNamespace("", null));
+		
+		assertTrue(Namespace.XML_NAMESPACE == Namespace.getNamespace(
+				XMLConstants.XML_NS_PREFIX, XMLConstants.XML_NS_URI));
+		
+		try {
+			assertTrue(null != Namespace.getNamespace("xml", "myuri"));
+			fail("Should not be able to re-brand Namespace prefix 'xml'");
+		} catch (IllegalNameException ine) {
+			// good
+		} catch (Exception e) {
+			fail("expected IllegalNameException, not " + e.getClass().getName());
+		}
+		
+		try {
+			assertTrue(null != Namespace.getNamespace("pfx", XMLConstants.XML_NS_URI));
+			fail("Should not be able to re-brand xml namespace URI to another prefix too.");
+		} catch (IllegalNameException ine) {
+			// good
+		} catch (Exception e) {
+			fail("expected IllegalNameException, not " + e.getClass().getName());
+		}
+		
+		try {
+			assertTrue(null != Namespace.getNamespace("pfx", "-this is illegal..."));
+			fail("Should not be able to have a URI starting with '-'.");
+		} catch (IllegalNameException ine) {
+			// good
+		} catch (Exception e) {
+			fail("expected IllegalNameException, not " + e.getClass().getName());
+		}
+		
+		try {
+			assertTrue(null != Namespace.getNamespace("p:x", "myuri"));
+			fail("Should not be able to create Namespace with illegal prefix 'xml'");
+		} catch (IllegalNameException ine) {
+			// good
+		} catch (Exception e) {
+			fail("expected IllegalNameException, not " + e.getClass().getName());
+		}
+		
+		try {
+			assertTrue(null != Namespace.getNamespace("pfx", " "));
+			fail("Should not be able to create Namespace with no URI");
+		} catch (IllegalNameException ine) {
+			// good
+		} catch (Exception e) {
+			fail("expected IllegalNameException, not " + e.getClass().getName());
+		}
+		
+		try {
+			assertTrue(null != Namespace.getNamespace("pfx", null));
+			fail("Should not be able to create Namespace with no URI");
+		} catch (IllegalNameException ine) {
+			// good
+		} catch (Exception e) {
+			fail("expected IllegalNameException, not " + e.getClass().getName());
+		}
+		
 	}
 
-    /**
+	/**
 	 * Test getPrefix()
 	 */
-    @Test
+	@Test
 	public void test_TCM__String_getPrefix() {
 		Namespace ns = Namespace.getNamespace("prefx","http://foo");
 		assertTrue("Incorrect namespace prefix", ns.getPrefix().equals("prefx"));
@@ -169,20 +237,20 @@ public final class TestNamespace {
 
 	}
 
-    /**
+	/**
 	 * Test than a namespace returns the correct URI
 	 */
-    @Test
+	@Test
 	public void test_TCM__String_getURI() {
 		Namespace ns = Namespace.getNamespace("prefx","http://foo");
 		assertTrue("Incorrect namespace prefix", ns.getURI().equals("http://foo"));
 
 	}
 
-    /**
+	/**
 	 * Test that toString() operates according to JDOM specs
 	 */
-    @Test
+	@Test
 	public void test_TCM__String_toString() {
 		Namespace ns = Namespace.getNamespace("http://some.new.place");
 		assertTrue("Incorrect namespace created", ns.toString().equals("[Namespace: prefix \"\" is mapped to URI \"http://some.new.place\"]"));
