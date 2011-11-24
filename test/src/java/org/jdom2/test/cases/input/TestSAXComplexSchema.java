08aeb4e3d4b6fb39207e76fa91f39e7550913e0c
Rolf <rolf@tuis.net>
2011-11-23 20:21:16 -0500

    Remove last vestiges of pre-JUnit4 code

--- test/src/java/org/jdom2/test/cases/input/TestSAXComplexSchema.java
+++ test/src/java/org/jdom2/test/cases/input/TestSAXComplexSchema.java
@@ -10,46 +10,75 @@ import java.net.URL;
 import java.util.Iterator;
 import java.util.List;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 import org.jdom2.*;
 import org.jdom2.input.SAXBuilder;
+import org.jdom2.input.sax.XMLReaderJAXPSingletons;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 /**
- * @author rlear
+ * @author Rolf Lear
  *
  */
 @SuppressWarnings("javadoc")
-public class TestSAXComplexSchema extends TestCase {
+public class TestSAXComplexSchema {
 
-	
-    /**
-     * The main method runs all the tests in the text ui
-     */
-    public static void main (String args[]) 
-     {
-        junit.textui.TestRunner.run(suite());
-    }
-
-    /**
-     * The suite method runs all the tests
-     */
-    public static Test suite () {
-        return new TestSuite(TestSAXComplexSchema.class);
-    }
 	
 	/**
 	 * Test method for {@link org.jdom.input.SAXBuilder#build(java.io.File)}.
 	 */
+	@Test
 	public void testBuildFile() throws IOException {
 		SAXBuilder builder = new SAXBuilder(true);
 		builder.setFeature("http://xml.org/sax/features/namespaces", true);
 		builder.setFeature("http://xml.org/sax/features/namespace-prefixes", true);
 		builder.setFeature("http://apache.org/xml/features/validation/schema", true);
+		
+		File inputdir = new File(".");
+		URL furl = inputdir.toURI().toURL();
+		URL rurl = new URL(furl, "test/resources/xsdcomplex/input.xml");
+		
+		
+		try {
+			Document doc = builder.build(rurl);
+			XMLOutputter out = new XMLOutputter(Format.getPrettyFormat());
+			StringWriter sw = new StringWriter();
+			out.output(doc, sw);
+			assertTrue(sw.toString().length() > 0);
+			//System.out.println("Document parsed. Content:\n" + xml + "\n");
+			
+			Namespace defns = Namespace.getNamespace("http://www.jdom.org/tests/default");
+			Namespace impns = Namespace.getNamespace("http://www.jdom.org/tests/imp");
+			
+			Element root = doc.getRootElement();
+			assertTrue(root != null);
+			assertTrue("test".equals(root.getName()));
+			List<Element> kids = root.getChildren("data", defns);
+			for (Iterator<Element> it = kids.iterator(); it.hasNext(); ) {
+				Element data = it.next();
+				assertTrue(defns.equals(data.getNamespace()));
+				Attribute att = data.getAttribute("type", Namespace.NO_NAMESPACE);
+				assertTrue("Could not find type attribute in default ns.", att != null);
+				att = data.getAttribute("type", impns);
+				assertTrue("Could not find type attribute in impns.", att != null);
+			}
+		} catch (JDOMException e) {
+			e.printStackTrace();
+			fail("Parsing failed. See stack trace.");
+		}
+		
+	}
+
+	/**
+	 * Test method for {@link org.jdom.input.SAXBuilder#build(java.io.File)}.
+	 */
+	@Test
+	public void testBuildFileNewSAX() throws IOException {
+		SAXBuilder builder = new SAXBuilder(XMLReaderJAXPSingletons.XSDVALIDATING);
 		
 		File inputdir = new File(".");
 		URL furl = inputdir.toURI().toURL();
