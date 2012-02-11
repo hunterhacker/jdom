dca30ff0c36ced09b06b6acbd62ae9551978b3a8
Rolf <rolf@tuis.net>
2012-02-11 17:02:39 -0500

    Fixes #62 - wrong NSURI for XMLNS

--- core/src/java/org/jdom2/JDOMConstants.java
+++ core/src/java/org/jdom2/JDOMConstants.java
@@ -48,7 +48,7 @@ public final class JDOMConstants {
 	/** Defined as {@value} */
 	public static final String NS_PFX_XMLNS = "xmlns";
 	/** Defined as {@value} */
-	public static final String NS_URI_XMLNS = "http://www.w3.org/XML/1998/namespace";
+	public static final String NS_URI_XMLNS = "http://www.w3.org/2000/xmlns/";
 	
 	
 	/** Defined as {@value} */
