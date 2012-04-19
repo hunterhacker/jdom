65084669db1003a51f574044cd08a56f0f056b94
Rolf <rolf@tuis.net>
2012-04-18 21:14:42 -0400

    Issue #75 - Android Update the build process to include the 'android' target which loads an Android main and test project with the JDOM unit tests. See https://github.com/hunterhacker/jdom/wiki/JDOM2-and-Android

---
+++ contrib/src/java/org/jdom2/contrib/android/TranslateTests.java
@@ -0,0 +1,327 @@
+package org.jdom2.contrib.android;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+
+/**
+ * This class is a translation layer that  translates JUnit4 tests to
+ * JUnit3 tests suitable for running with the Android Test harness.
+ * <p/>
+ * This class is a small part of an automated process described in the
+ * wiki page https://github.com/hunterhacker/jdom/wiki/JDOM2-and-Android
+ * 
+ * @author Rolf Lear
+ *
+ */
+
+@SuppressWarnings("javadoc")
+public class TranslateTests {
+	
+	private static final String[] skipclasses = new String[] {".*StAX.*"};
+	
+	private static final String[] skipmethods = new String[] {".*HighSurrogateAttPair.*"};
+
+	private static final Pattern pat = Pattern.compile("^(.+/(\\w+))\\.class$");
+
+	private static String potentialTest(String zename) {
+		final Matcher mat = pat.matcher(zename);
+		if (mat.matches()) {
+			final String cname = mat.group(2);
+			if (cname.startsWith("Test") || cname.endsWith("Test")) {
+				for (String cm : skipclasses) {
+					if (Pattern.matches(cm, cname)) {
+						return null;
+					}
+				}
+				final String cp = mat.group(1);
+				return cp.replace('/', '.');
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Convert the Jar file to a set of new JUnit3 tests.
+	 * @param args The two args are expected to be a Jar file (unit tests), and an output directory 
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		if (args.length != 3) {
+			throw new IllegalArgumentException("Usage: Jar AssetOutDir SrcOutDir");
+		}
+		final String jarname = args[0];
+		
+		File assoutdir = new File(args[1]);
+		if (!assoutdir.exists()) {
+			assoutdir.mkdirs();
+		}
+		if (!assoutdir.isDirectory()) {
+			throw new IllegalArgumentException("Could not create/use AssetOutput directory: " + assoutdir);
+		}
+		
+		final File srcoutdir = new File(args[2]);
+		if (!srcoutdir.exists()) {
+			srcoutdir.mkdirs();
+		}
+		if (!srcoutdir.isDirectory()) {
+			throw new IllegalArgumentException("Could not create/use SrcOutput directory: " + srcoutdir);
+		}
+		
+		final ArrayList<String> classes = new ArrayList<String>();
+		
+		final ZipFile zfile = new ZipFile(jarname);
+		StringBuilder assetnames = new StringBuilder();
+		try {
+			final Enumeration<? extends ZipEntry> e = zfile.entries();
+			while (e.hasMoreElements()) {
+				final ZipEntry ze = e.nextElement();
+				if (ze.isDirectory()) {
+					continue;
+				}
+				final String zename = ze.getName();
+				if (zename.endsWith(".class")) {
+					final String classname = potentialTest(zename);
+					if (classname != null) {
+						TranslateTests tt = new TranslateTests(srcoutdir, classname);
+						classes.add(tt.translate());
+					}
+				} else if (zename.indexOf("META-INF") < 0){
+					// copy resources in to output folder with full path.
+					System.out.println("Asset: " + zename);
+					assetnames.append(zename).append("\n");
+					final InputStream zis = zfile.getInputStream(ze);
+					final File outfile = new File(assoutdir, zename);
+					outfile.getParentFile().mkdirs();
+					final FileOutputStream fos = new FileOutputStream(outfile);
+					final byte[] buffer = new byte[4096];
+					int len = 0;
+					while ((len = zis.read(buffer)) >= 0) {
+						fos.write(buffer, 0, len);
+					}
+					fos.flush();
+					fos.close();
+					zis.close();
+				}
+			}
+			final FileWriter fw = new FileWriter(new File(assoutdir, "assetlist.txt"));
+			fw.write(assetnames.toString());
+			fw.flush();
+			fw.close();
+		} finally {
+			zfile.close();
+		}
+		
+	}
+
+	private final Class<?> tclass;
+	private final File outf;
+
+	public TranslateTests(File outdir, String zename) {
+		System.out.println("Contemplating " + zename);
+		try {
+			tclass = Class.forName(zename);
+		} catch (Exception e) {
+			throw new IllegalStateException("Unable to load class " + zename, e);
+		}
+		final String path = zename.replace('.', '/') + "TT.java";
+		outf = new File(outdir,path);
+	}
+	
+/*
+
+
+package com.example.android.skeletonapp.test;
+
+import org.jdom2.test.cases.TestElement;
+
+import android.test.AndroidTestCase;
+
+@SuppressWarnings("javadoc")
+public class JDOMMainTest extends AndroidTestCase {
+
+	
+	public void testElement() {
+		assertTrue(Integer.valueOf(1) > 0);
+		TestElement te = new TestElement();
+		te.testCoalesceTextNested();
+	}
+}
+
+
+ */
+
+	private String translate() throws IOException {
+		final String sname = tclass.getSimpleName();
+		final StringBuilder sb = new StringBuilder();
+		final String ret = tclass.getPackage().getName() + "." + sname + "TT";
+		
+		sb.append("package ").append(tclass.getPackage().getName()).append(";\n");
+		sb.append("@SuppressWarnings(\"javadoc\")\n");
+		sb.append("public class ").append(sname).append("TT extends android.test.AndroidTestCase {\n");
+		
+		if (tclass.getAnnotation(Ignore.class) != null) {
+			sb.append("\n\n    // Class has @Ignore set\n\n");
+		} else {
+		
+			final Method[] methods = tclass.getMethods();
+			
+			final ArrayList<Method> pretest = new ArrayList<Method>();
+			final ArrayList<Method> posttest = new ArrayList<Method>();
+			final TreeSet<String> excepts = new TreeSet<String>();
+			
+			for (Method m : methods) {
+				if (m.getAnnotation(Before.class) != null) {
+					pretest.add(m);
+					excepts.addAll(getExceptions(m));
+				}
+				if (m.getAnnotation(After.class) != null) {
+					posttest.add(m);
+					excepts.addAll(getExceptions(m));
+				}
+			}
+			
+			sb.append("  private final ").append(sname).append(" test = new ").append(sname).append("();\n");
+			
+			sb.append("\n  @Override\n");
+			sb.append("  public void setUp() {\n");
+			sb.append("    // tests run when class starts...\n");
+			sb.append("    org.jdom2.test.util.AndroidFetch.check(this.getContext());\n");
+			for (Method m : methods) {
+				if (m.getAnnotation(BeforeClass.class) != null) {
+					sb.append("    test.").append(m.getName()).append("();\n");
+				}
+			}
+			sb.append("  }\n");
+			
+			sb.append("\n  @Override\n");
+			sb.append("  public void tearDown() {\n");
+			sb.append("    // tests run when class completes...\n");
+			for (Method m : methods) {
+				if (m.getAnnotation(AfterClass.class) != null) {
+					sb.append("    test.").append(m.getName()).append("();\n");
+				}
+			}
+			sb.append("  }\n");
+			
+			for (Method m : methods) {
+				Test tanno = null;
+				if ((tanno = m.getAnnotation(Test.class)) != null && m.getAnnotation(Ignore.class) == null) {
+					final String tname = getTestName(m);
+					sb.append("\n");
+					sb.append("  public void ").append(tname).append("()").append(buildThrows(excepts, m)).append("{\n");
+					sb.append("    org.jdom2.test.util.AndroidFetch.check(this.getContext());\n");
+					for (Method pre : pretest) {
+						sb.append("    // pre test\n");
+						sb.append("    test.").append(pre.getName()).append("();\n");
+					}
+					if (posttest.isEmpty()) {
+						sb.append("    // actual test\n");
+						if (tanno.expected() == Test.None.class) {
+							sb.append("    test.").append(m.getName()).append("();\n");
+						} else {
+							sb.append("    try {\n");
+							sb.append("      test.").append(m.getName()).append("();\n");
+							sb.append("        org.jdom2.test.util.UnitTestUtil.failNoException(").append(tanno.expected().getName()).append(".class);\n");
+							sb.append("    } catch (").append(tanno.expected().getName()).append(" e) {\n");
+							sb.append("        org.jdom2.test.util.UnitTestUtil.checkException(").append(tanno.expected().getName()).append(".class, e);\n");
+							sb.append("    }\n");
+						}
+					} else {
+						sb.append("    try {\n");
+						sb.append("      // actual test\n");
+						if (tanno.expected() == Test.None.class) {
+							sb.append("      test.").append(m.getName()).append("();\n");
+						} else {
+							sb.append("      try {\n");
+							sb.append("        test.").append(m.getName()).append("();\n");
+							sb.append("        org.jdom2.test.util.UnitTestUtil.failNoException(").append(tanno.expected().getName()).append(".class);\n");
+							sb.append("      } catch (Exception e) {\n");
+							sb.append("        org.jdom2.test.util.UnitTestUtil.checkException(").append(tanno.expected().getName()).append(".class, e);\n");
+							sb.append("      }\n");
+						}
+						sb.append("    } finally {\n");
+						for (Method post : posttest) {
+							sb.append("    // post test\n");
+							sb.append("    test.").append(post.getName()).append("();\n");
+						}
+						sb.append("    }\n");
+						
+					}
+					sb.append("  }\n");
+				}
+			}
+			
+		}
+		
+		sb.append("\n}\n");
+		final File outd = outf.getParentFile().getAbsoluteFile();
+		if (!outd.isDirectory()) {
+			outd.mkdirs();
+		}
+		FileWriter fw = new FileWriter(outf);
+		fw.write(sb.toString());
+		fw.flush();
+		fw.close();
+		return ret;
+	}
+
+	private String getTestName(final Method m) {
+		final String mname = m.getName();
+		for (String mm : skipmethods) {
+			if (Pattern.matches(mm, mname)) {
+				return "/* Skip test on Android */ do_not_" + mname;
+			}
+		}
+		return mname.startsWith("test") ? mname : ("test_" + mname);
+	}
+
+	private Set<String> getExceptions(Method m) {
+		TreeSet<String> hs = new TreeSet<String>();
+		if (m != null) {
+			for (Class<?> ec : m.getExceptionTypes()) {
+				hs.add(ec.getName());
+			}
+		}
+		return hs;
+	}
+	
+	private String buildThrows(Set<String> current, Method m) {
+		final Set<String> tothrow = getExceptions(m);
+		if (current != null) {
+			tothrow.addAll(current);
+		}
+		final Iterator<String> it = tothrow.iterator();
+		if (!it.hasNext()) {
+			return " ";
+		}
+		final StringBuilder sb = new StringBuilder();
+		sb.append(" throws ").append(it.next());
+		while (it.hasNext()) {
+			sb.append(", ").append(it.next());
+		}
+		sb.append(" ");
+		return sb.toString();
+	}
+	
+}
---
+++ test/android/AndroidFetch.java
@@ -0,0 +1,129 @@
+package org.jdom2.test.util;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReference;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+/**
+ * This class is a hack class that adapts the JDOM Unit test environment to work on android.
+ * See the wiki page: https://github.com/hunterhacker/jdom/wiki/JDOM2-and-Android
+ * 
+ * @author Rolf Lear
+ *
+ */
+public class AndroidFetch extends FidoFetch{
+	
+	private final AtomicReference<File> folder = new AtomicReference<File>();
+	
+	private void writeCache(File cf, byte[] data) throws IOException {
+		final File d = cf.getParentFile();
+		if (!d.exists()) {
+			d.mkdirs();
+		}
+		final FileOutputStream fos = new FileOutputStream(cf);
+		fos.write(data);
+		fos.flush();
+		fos.close();
+	}
+	
+	private byte[] readAsset(AssetManager am, String name) throws IOException {
+		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		final InputStream is = am.open(name);
+		final byte[] buffer = new byte[2048];
+		int len = 0;
+		while ((len = is.read(buffer)) >= 0) {
+			baos.write(buffer, 0, len);
+		}
+		is.close();
+		return baos.toByteArray();
+	}
+
+	private String[] parseAssets(byte[] data) throws IOException {
+		final BufferedReader br = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(data)));
+		final ArrayList<String> al = new ArrayList<String>();
+		String line = null;
+		while ((line = br.readLine()) != null) {
+			if (line.trim().length() > 0) {
+				al.add(line.trim());
+			}
+		}
+		return al.toArray(new String[al.size()]);
+	}
+
+
+	/**
+	 * @param o This should be a context object
+	 */
+	public static final void check(Context o) {
+		final FidoFetch ff = getFido();
+		final AndroidFetch af = (AndroidFetch)ff;
+		af.checkFolder(o);
+		
+	}
+
+	private void checkFolder(Context ctx) {
+		if (folder.get() == null) {
+			// first time in....
+			
+			// OK, now set the cache folder.
+			final File dir = new File(ctx.getCacheDir(), "jdomassets");
+			if (!dir.exists()) {
+				dir.mkdirs();
+			}
+			
+			// Then, get the Asset Manager.
+			final AssetManager am = ctx.getAssets();
+			
+			try {
+			
+				// we want the special asset 'assetlist.txt' which is created by the
+				// JDOM 'android' build target, listing all the required assets.
+				final String[] assets = parseAssets(readAsset(am, "assetlist.txt"));
+				for (String asset : assets) {
+					final byte[] data = readAsset(am, asset);
+					final File f = new File(dir, asset);
+					writeCache(f, data);
+					Log.i("JDOM", "Transfer asset " + asset + " to cache.");
+				}
+			} catch (Exception e) {
+				throw new IllegalStateException("Unable to transfer assets to " + dir, e);
+			}
+			
+			folder.set(dir);
+		}
+	}
+
+	@Override
+	public URL getURL(String name) {
+		try {
+			final File f = new File(folder.get(), name);
+			return f.toURI().toURL();
+		} catch (Exception e) {
+			throw new IllegalStateException("Unable to get resource " + name, e);
+		}
+	}
+
+	@Override
+	public InputStream getStream(String name) {
+		try {
+			final File f = new File(folder.get(), name);
+			return new FileInputStream(f);
+		} catch (Exception e) {
+			throw new IllegalStateException("Unable to get resource " + name, e);
+		}
+	}
+	
+}
