880a8aa165fd65f87450c9f2a4cde6744c104458
Rolf <rolf@tuis.net>
2013-04-14 17:18:55 -0400

    Issue #106 - This is a more correct fix. StAX is a *poor* specification, and the default implementations of StAX are broken too.

--- core/src/java/org/jdom2/output/support/AbstractStAXStreamProcessor.java
+++ core/src/java/org/jdom2/output/support/AbstractStAXStreamProcessor.java
@@ -66,6 +66,7 @@ import org.jdom2.Attribute;
 import org.jdom2.CDATA;
 import org.jdom2.Comment;
 import org.jdom2.Content;
+import org.jdom2.JDOMConstants;
 import org.jdom2.Content.CType;
 import org.jdom2.DocType;
 import org.jdom2.Document;
@@ -552,8 +553,17 @@ public abstract class AbstractStAXStreamProcessor
 	protected void printElement(final XMLStreamWriter out, final FormatStack fstack,
 			final NamespaceStack nstack, final Element element) throws XMLStreamException {
 
+		final ArrayList<String> restore = new ArrayList<String>();
 		nstack.push(element);
 		try {
+			for (Namespace nsa : nstack.addedForward()) {
+				restore.add(nsa.getPrefix());
+				if (JDOMConstants.NS_PREFIX_DEFAULT.equals(nsa.getPrefix())) {
+					out.setDefaultNamespace(nsa.getURI());
+				} else {
+					out.setPrefix(nsa.getPrefix(), nsa.getURI());
+				}
+			}
 			
 			final List<Content> content = element.getContent();
 
@@ -601,17 +611,9 @@ public abstract class AbstractStAXStreamProcessor
 			// then we must expand.
 			boolean expandit = walker != null || fstack.isExpandEmptyElements();
 			
+			final Namespace ns = element.getNamespace();
 			if (expandit) {
-				Namespace ns = element.getNamespace();
-//				out.setPrefix(ns.getPrefix(), ns.getURI());
 				out.writeStartElement(ns.getPrefix(), element.getName(), ns.getURI());
-//				if (ns == Namespace.NO_NAMESPACE) {
-//					out.writeStartElement(element.getName());
-//				} else if ("".equals(ns.getPrefix())) {
-//					out.writeStartElement(ns.getURI(), element.getName());
-//				} else {
-//					out.writeStartElement(ns.getPrefix(), element.getName(), ns.getURI());
-//				}
 				
 				// Print the element's namespace, if appropriate
 				for (final Namespace nsd : nstack.addedForward()) {
@@ -625,6 +627,11 @@ public abstract class AbstractStAXStreamProcessor
 					}
 				}
 				
+				// This neatens up the output stream for some reason - bug in standard StAX
+				// implementation requires us to close off the Element start tag before we
+				// start adding new Namespaces to child contexts...
+				out.writeCharacters("");
+
 				// OK, now we print out the meat of the Element
 				if (walker != null) {
 					// we need to re-create the walker/fstack.
@@ -651,7 +658,6 @@ public abstract class AbstractStAXStreamProcessor
 			
 				out.writeEndElement();
 				
-				
 			} else {
 				// implies:
 				//      fstack.isExpandEmpty... is false
@@ -660,16 +666,7 @@ public abstract class AbstractStAXStreamProcessor
 				//           and preserve == false
 				//           and whiteonly == true
 				
-				Namespace ns = element.getNamespace();
-//				out.setPrefix(ns.getPrefix(), ns.getURI());
 				out.writeEmptyElement(ns.getPrefix(), element.getName(), ns.getURI());
-//				if (ns == Namespace.NO_NAMESPACE) {
-//					out.writeEmptyElement(element.getName());
-//				} else if ("".equals(ns.getPrefix())) {
-//					out.writeEmptyElement("", element.getName(), ns.getURI());
-//				} else {
-//					out.writeEmptyElement(ns.getPrefix(), element.getName(), ns.getURI());
-//				}
 				
 				// Print the element's namespace, if appropriate
 				for (final Namespace nsd : nstack.addedForward()) {
@@ -680,13 +677,25 @@ public abstract class AbstractStAXStreamProcessor
 				for (final Attribute attribute : element.getAttributes()) {
 					printAttribute(out, fstack, attribute);
 				}
-
 				// This neatens up the output stream for some reason.
 				out.writeCharacters("");
 			}
-		
+
 		} finally {
 			nstack.pop();
+			for (String pfx : restore) {
+				for (final Namespace nsa : nstack) {
+					if (nsa.getPrefix().equals(pfx)) {
+						if (JDOMConstants.NS_PREFIX_DEFAULT.equals(nsa.getPrefix())) {
+							out.setDefaultNamespace(nsa.getURI());
+						} else {
+							out.setPrefix(nsa.getPrefix(), nsa.getURI());
+						}
+						break;
+					}
+				}
+			}
+			
 		}
 	}
 
